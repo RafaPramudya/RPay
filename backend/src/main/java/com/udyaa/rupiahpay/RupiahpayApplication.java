71f00b8681361e3ade12df73caf5c571d83d03c2
RafaPramudya <bedsetmodern@gmail.com>
2026-03-24 17:58:42 +0700

    Add Docker Compose Build and Update pom.xml

--- backend/src/main/java/com/udyaa/rupiahpay/RupiahpayApplication.java
+++ backend/src/main/java/com/udyaa/rupiahpay/RupiahpayApplication.java
@@ -3,17 +3,10 @@ package com.udyaa.rupiahpay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class RupiahpayApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
-
 		SpringApplication.run(RupiahpayApplication.class, args);
 	}
 
